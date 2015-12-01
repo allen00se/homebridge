bf3f48cce8c15d5850e6f787d9176640cef55821
allen00se <allen.krys@gmail.com>
2015-12-01 13:58:30 -0600

    Update SmartThingsHelloHome.js

--- accessories/SmartThingsHelloHome.js
+++ accessories/SmartThingsHelloHome.js
@@ -1,4 +1,4 @@
-var types  = require("../lib/HAP-NodeJS/accessories/types.js")
+var types = require("../api").homebridge.hapLegacyTypes;
 var request = require("request")
 
 function SmartThingsHelloHomeAccessory(log, config) {
